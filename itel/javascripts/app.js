a61784fc20694fab9274ff75437723409c7c70fa
michaeljfast <michaeljfast@gmail.com>
2017-01-22 21:21:42 -0800

    Final copy, added some error checking and rewrote pagination function

--- itel/javascripts/app.js
+++ itel/javascripts/app.js
@@ -9,8 +9,14 @@ angular.module('myApp', [])
 	$scope.city = 'Kamloops';
 	
 	function fetch() {
+		$scope.error = false;
 		$http.get("http://api.openweathermap.org/data/2.5/forecast/daily?q=" + $scope.city + ",CA&cnt=16&mode=json&units=metric&APPID=1ae28c1aff5030e36f9e541fd6c02940")
-		.then(function(response) { $scope.days = response.data; });
+		.then(function(response) { $scope.days = response.data; },
+		function() { $scope.error = true }
+	)};
+	
+	$scope.fetch = fetch;
+	
 	}
 	
 	$scope.select = function(){
@@ -25,9 +31,9 @@ angular.module('myApp', [])
 			this.daysDisplayed = 5;
 			this.daysStartIndex = 0;
 			this.paginate = function(direction) {
-				this.daysStartIndex += parseInt(direction, 10);
-				this.daysStartIndex = (this.daysStartIndex <= 0 ? 0 : this.daysStartIndex);
-				this.daysStartIndex = (this.daysStartIndex >= (this.data.list.length -1) ? (this.data.list.length -5) : this.daysStartIndex);
+				var lastListIndex = this.data.list.length -1;
+				var newStartIndex = this.daysStartIndex + direction;
+				this.daysStartIndex = Math.min(Math.max(0, newStartIndex), lastListIndex);
 			}
 		},
 		templateUrl: 'partials/daysComponent.html'
